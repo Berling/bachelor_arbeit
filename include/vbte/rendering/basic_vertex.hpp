670b9f67e6898bfa88b1d9339af1ea77f5cee180
Georg Schäfer <schaefg@hochschule-trier.de>
2015-11-06 09:20:34 +0100

    add basic vertex struct

---
+++ include/vbte/rendering/basic_vertex.hpp
@@ -0,0 +1,14 @@
+#ifndef __VBTE_RENDRING_BASIC_VERTEX_HPP__
+#define __VBTE_RENDRING_BASIC_VERTEX_HPP__
+
+#include <glm/glm.hpp>
+
+namespace vbte {
+	namespace rendering {
+		struct basic_vertex {
+			glm::vec3 position;
+		};
+	}
+}
+
+#endif
