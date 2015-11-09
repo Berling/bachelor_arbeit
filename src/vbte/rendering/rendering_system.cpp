5c2dac33247f47617d5ff48af253d05298f35d4e
Georg Schäfer <schaefg@hochschule-trier.de>
2015-11-09 10:26:30 +0100

    add test draw to rendering system

--- src/vbte/rendering/rendering_system.cpp
+++ src/vbte/rendering/rendering_system.cpp
@@ -1,8 +1,11 @@
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <vbte/core/engine.hpp>
 #include <vbte/graphics/graphics_system.hpp>
 #include <vbte/graphics/shader.hpp>
 #include <vbte/graphics/shader_manager.hpp>
 #include <vbte/rendering/basic_vertex.hpp>
+#include <vbte/rendering/cube.hpp>
 #include <vbte/rendering/rendering_system.hpp>
 
 namespace vbte {
@@ -32,7 +35,35 @@ namespace vbte {
 		}
 
 		void rendering_system::update(float delta_time) {
+			static cube c{engine_, glm::vec3{1.f}};
+			
+			glEnable(GL_DEPTH_TEST);
 
+			debug_program_.use();
+			debug_program_.uniform("projection", false, glm::perspective(glm::radians(45.f), 4.f / 3.f, 0.1f, 1000.f));
+			debug_program_.uniform("view", false, glm::lookAt(glm::vec3{2.f, 3.f, 5.f}, glm::vec3{0.f}, glm::vec3{0.f, 1.f, 0.f}));
+			debug_program_.uniform("model", false, glm::mat4{1.f});
+			debug_program_.uniform("color", debug_edge_color_);
+			glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+			glPointSize(8.f);
+			c.draw();
+
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			glLineWidth(3.f);
+			c.draw();
+
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			glEnable(GL_POLYGON_OFFSET_FILL);
+			glPolygonOffset(1.f, 1.f);
+			glCullFace(GL_FRONT);
+
+			auto clear_color = engine_.graphics_system().clear_color();
+			debug_program_.uniform("color", debug_face_color_);//glm::vec3{clear_color});
+			c.draw();
+
+			glDisable(GL_POLYGON_OFFSET_FILL);
+
+			glDisable(GL_DEPTH_TEST);
 		}
 	}
 }
