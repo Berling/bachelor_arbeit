8a2f2fa7d67e5a753142a6e58cc35e6710d12622
Georg Schäfer <schaefg@hochschule-trier.de>
2015-11-12 12:04:44 +0100

    add shading to wireframe filled draw mode

--- src/vbte/rendering/rendering_system.cpp
+++ src/vbte/rendering/rendering_system.cpp
@@ -14,7 +14,7 @@
 namespace vbte {
 	namespace rendering {
 		rendering_system::rendering_system(core::engine& engine)
-		: engine_{engine}, debug_face_color_{0.f, 0.59f, 0.93f}, debug_edge_color_{0.09f, 0.19f, 0.49f},
+		: engine_{engine}, debug_face_color_{0.f, 0.59f, 0.93f}, debug_edge_color_{1.f},
 		mode_{rendering_mode::shaded} {
 			basic_layout_.emplace_back("_position", 3, GL_FLOAT, false, sizeof(basic_vertex), offsetof(basic_vertex, position));
 			basic_layout_.emplace_back("_normal", 3, GL_FLOAT, false, sizeof(basic_vertex), offsetof(basic_vertex, normal));
@@ -65,12 +65,9 @@ namespace vbte {
 				for (auto& geometry : draw_queue_) {
 					debug_program_.uniform("color", debug_edge_color_);
 					debug_program_.uniform("model", false, geometry->transform());
-					glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-					glPointSize(8.f);
-					geometry->draw();
 
 					glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-					glLineWidth(3.f);
+					glLineWidth(1.f);
 					geometry->draw();
 
 					glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -85,19 +82,15 @@ namespace vbte {
 					glDisable(GL_POLYGON_OFFSET_FILL);
 				}
 			} else if (mode_ == rendering_mode::wireframe_filled) {
-				debug_program_.use();
-				debug_program_.uniform("projection", false, camera.projection());
-				debug_program_.uniform("view", false, camera.view());
-				
 				for (auto& geometry : draw_queue_) {
+					debug_program_.use();
+					debug_program_.uniform("projection", false, camera.projection());
+					debug_program_.uniform("view", false, camera.view());
 					debug_program_.uniform("color", debug_edge_color_);
 					debug_program_.uniform("model", false, geometry->transform());
-					glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-					glPointSize(8.f);
-					geometry->draw();
 
 					glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-					glLineWidth(3.f);
+					glLineWidth(1.f);
 					geometry->draw();
 
 					glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -106,7 +99,18 @@ namespace vbte {
 					glCullFace(GL_FRONT);
 
 					auto clear_color = engine_.graphics_system().clear_color();
-					debug_program_.uniform("color", debug_face_color_);
+					light_program_.use();
+					light_program_.uniform("projection", false, camera.projection());
+					light_program_.uniform("view", false, camera.view());
+					light_program_.uniform("model", false, geometry->transform());
+					light_program_.uniform("mit", false, glm::inverseTranspose(geometry->transform()));
+					light_program_.uniform("view_vector", glm::vec3{2.f, 3.f, 5.f});
+					light_program_.uniform("color", debug_face_color_);
+					light_program_.uniform("light_direction", glm::vec3{0.2f, 0.3f, 1.f});
+					light_program_.uniform("light_color", glm::vec3{1.f});
+					light_program_.uniform("light_energy", 1.0f);
+					light_program_.uniform("ambient_term", glm::vec3{0.1f});
+					light_program_.uniform("color", debug_face_color_);
 					geometry->draw();
 
 					glDisable(GL_POLYGON_OFFSET_FILL);
