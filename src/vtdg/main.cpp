d5bc0edb213652b3bcc156358329e3c60ee729cc
Georg Schäfer <schaefg@hochschule-trier.de>
2016-01-14 02:11:50 +0100

    add volume data generation test with sphere

--- src/vtdg/main.cpp
+++ src/vtdg/main.cpp
@@ -1,3 +1,27 @@
+#include <string>
+
+#include <vtdg/uniform_grid.hpp>
+
 int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		throw std::runtime_error{"illegal number of command line arguments"};
+	}
+
+	vtdg::uniform_grid grid{5.f, 128};
+
+	auto ball = [](const glm::vec3& p) {
+		static auto center = glm::vec3{2.5f};
+		static auto radius = 1.5f;
+
+		if (glm::dot(p - center, p - center) < radius * radius) {
+			return 1.f;
+		} else {
+			return 0.f;
+		}
+	};
+
+	grid.fill(ball);
+	grid.serialize(argv[1]);
+
 	return 0;
 }
