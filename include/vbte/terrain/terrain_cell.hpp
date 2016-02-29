cbd424b13ecbc7b560a43d39b21cc81675a67b94
Georg Schäfer <schaefg@hochschule-trier.de>
2016-02-29 15:41:23 +0100

    Add drawable for rendering terrain cells.

---
+++ include/vbte/terrain/terrain_cell.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+#include <glm/glm.hpp>
+
+#include <vbte/graphics/buffer.hpp>
+#include <vbte/graphics/vertex_array.hpp>
+#include <vbte/rendering/drawable.hpp>
+
+namespace vbte {
+  namespace core {
+    class engine;
+  }
+}
+
+namespace vbte {
+  namespace terrain {
+    class terrain_cell : public rendering::drawable {
+    private:
+      graphics::vertex_array vao_;
+      graphics::vertex_buffer vbo_;
+      size_t index_count;
+
+    public:
+      terrain_cell(core::engine& engine, const glm::vec3& position, const glm::quat& rotation, const std::string& file_name);
+      ~terrain_cell() = default;
+
+      void draw() const override;
+    };
+  }
+}
