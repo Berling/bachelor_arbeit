2c7ff79d857fd443e67635196bb2d105b919aa2a
Georg Schäfer <schaefg@hochschule-trier.de>
2016-03-29 14:27:30 +0200

    Add command line options to volume test data generator for grid length, resolution and sampling function.

--- src/vtdg/main.cpp
+++ src/vtdg/main.cpp
@@ -5,15 +5,18 @@
 #include <vtdg/uniform_grid.hpp>
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
+	if (argc != 5) {
 		throw std::runtime_error{"illegal number of command line arguments"};
 	}
 
-	vtdg::uniform_grid grid{5.f, 64};
+	auto grid_length = std::stof(argv[1]);
+	auto grid_resolution = std::stoul(argv[2]);
 
-	auto ball = [](const glm::vec3& p) {
-		static auto center = glm::vec3{2.5f};
-		static auto radius = 1.5f;
+	vtdg::uniform_grid grid{grid_length, grid_resolution};
+
+	auto sphere = [&](const glm::vec3& p) {
+		static auto center = glm::vec3{grid_length / 2.f};
+		static auto radius = grid_length / 3.f;
 
 		if (glm::dot(p - center, p - center) < radius * radius) {
 			return 1.f;
@@ -22,12 +25,19 @@ int main(int argc, char* argv[]) {
 		}
 	};
 
-	auto noise = [](const glm::vec3& p) {
+	auto noise = [&](const glm::vec3& p) {
 		return 5.f * vtdg::perlin_noise::noise(p.x, p.y, p.z);
 	};
 
-	grid.fill(noise);
-	grid.serialize(argv[1]);
+	auto sampling_function = std::string{argv[3]};
+	if (sampling_function == "sphere") {
+		grid.fill(sphere);
+	} else if (sampling_function == "noise") {
+		grid.fill(noise);
+	} else {
+		throw std::runtime_error{"no sampling function named " + sampling_function};
+	}
+	grid.serialize(argv[4]);
 
 	return 0;
 }
