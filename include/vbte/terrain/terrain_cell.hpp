2d992a6125f126c0c63f18bd45793579cdb9ab5c
Georg Schäfer <schaefg@hochschule-trier.de>
2016-03-29 16:06:55 +0200

    Save volume data in terrain cell.

--- include/vbte/terrain/terrain_cell.hpp
+++ include/vbte/terrain/terrain_cell.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 #include <glm/glm.hpp>
@@ -12,15 +13,20 @@ namespace vbte {
 	namespace core {
 		class engine;
 	}
+
+	namespace terrain {
+		class volume_data;
+	}
 }
 
 namespace vbte {
 	namespace terrain {
 		class terrain_cell : public rendering::drawable {
 		private:
+			std::shared_ptr<const volume_data> volume_data_;
 			graphics::vertex_array vao_;
 			graphics::vertex_buffer vbo_;
-			size_t index_count;
+			size_t index_count_;
 
 		public:
 			terrain_cell(core::engine& engine, const glm::vec3& position, const glm::quat& rotation, const std::string& file_name);
--- src/vbte/terrain/terrain_cell.cpp
+++ src/vbte/terrain/terrain_cell.cpp
@@ -13,21 +13,21 @@ namespace vbte {
 	namespace terrain {
 		terrain_cell::terrain_cell(core::engine& engine, const glm::vec3& position, const glm::quat& rotation, const std::string& file_name) :
 		rendering::drawable{engine, position, rotation}, vbo_{GL_DYNAMIC_DRAW} {
-			auto volume_data = engine_.terrain_system().volume_data_manager().load(file_name);
-			if (!volume_data) {
+			volume_data_ = engine_.terrain_system().volume_data_manager().load(file_name);
+			if (!volume_data_) {
 				throw std::runtime_error{"could not load file " + file_name};
 			}
 
-			auto vertices = marching_cubes(*volume_data, volume_data->resolution());
-			index_count = vertices.size();
-			vbo_.data(sizeof(rendering::basic_vertex) * index_count, vertices.data());
+			auto vertices = marching_cubes(*volume_data_, volume_data_->resolution());
+			index_count_ = vertices.size();
+			vbo_.data(sizeof(rendering::basic_vertex) * index_count_, vertices.data());
 
 			engine_.rendering_system().basic_layout().setup_layout(vao_, &vbo_);
 		}
 
 		void terrain_cell::draw() const {
 			vao_.bind();
-			glDrawArrays(GL_TRIANGLES, 0, index_count);
+			glDrawArrays(GL_TRIANGLES, 0, index_count_);
 		}
 	}
 }
