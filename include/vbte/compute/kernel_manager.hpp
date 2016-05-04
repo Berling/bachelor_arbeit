89586af7503b61f80dda383115c657274580f4e8
Georg Schäfer <schaefg@hochschule-trier.de>
2016-05-04 16:06:43 +0200

    Add compute kernel manager.

---
+++ include/vbte/compute/kernel_manager.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <vbte/compute/kernel.hpp>
+#include <vbte/utils/cached_resource_manager.hpp>
+
+namespace vbte {
+	namespace core {
+		class engine;
+	}
+
+	namespace terrain {
+		class terrain_system;
+	}
+}
+
+namespace vbte {
+	namespace compute {
+		using kernel_ptr = std::shared_ptr<const kernel>;
+
+		class kernel_manager : public utils::cached_resource_manager<const kernel, kernel_manager> {
+		private:
+			core::engine& engine_;
+			terrain::terrain_system& terrain_system_;
+
+			friend class utils::cached_resource_manager<const kernel, kernel_manager>;
+
+		public:
+			kernel_manager(core::engine& engine, terrain::terrain_system& terrain_system) noexcept;
+			~kernel_manager() = default;
+
+		protected:
+			kernel_ptr load_new(const std::string& file_name, const std::string& kernel_name);
+		};
+	}
+}
---
+++ src/vbte/compute/kernel_manager.cpp
@@ -0,0 +1,22 @@
+#include <GL/glew.h>
+
+#include <vbte/asset/asset.hpp>
+#include <vbte/compute/kernel_manager.hpp>
+#include <vbte/core/engine.hpp>
+#include <vbte/terrain/terrain_system.hpp>
+
+namespace vbte {
+	namespace compute {
+		kernel_manager::kernel_manager(core::engine& engine, terrain::terrain_system& terrain_system) noexcept
+		: engine_{engine}, terrain_system_(terrain_system) {}
+
+		kernel_ptr kernel_manager::load_new(const std::string& file_name, const std::string& kernel_name) {
+			auto asset = engine_.asset_manager().load(file_name);
+			if (!asset) {
+				return nullptr;
+			}
+			auto source_ptr = std::string(asset->content().begin(), asset->content().end());
+			return std::make_shared<const kernel>(terrain_system_.compute_context(), kernel_name, source_ptr);
+		}
+	}
+}
